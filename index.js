32f041d4159e83c0fee7ca4b4f839fb3a15a4dd8
Swayam Pandya <swayampandya1236@gmail.com>
2023-05-02 23:19:37 +0530

    Img added and script updated

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("new node");
+const handleEvent = (e) => {
+    var inputValue = e.inputbox.value;
+    document.getElementById("maincont").style.opacity = "0";
+    document.getElementById("evaluate").style.zIndex = "100";
+
+    const loader = ['Initializating Reader', 'Processing Input', "Calculating Possible Outcomes" , "Generating Response"]
+    const loaderText = document.getElementById('loaderText');
+    loaderText.innerHTML = "Initializating Reader";
+    const myTimeout = setTimeout(secondAgrument, 1500);
+    function secondAgrument() {
+        loaderText.innerHTML = "Processing Input"
+      }
+    const my2ndTimeout = setTimeout(thirdAgrument, 1500);
+    function thirdAgrument() {
+        loaderText.innerHTML = "Processing Input"
+    }
+    const my3rdTimeout = setTimeout(fourthAgrument, 1500);
+    function fourthAgrument() {
+        loaderText.innerHTML = "Processing Input"
+    }
+    
+}
